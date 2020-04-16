5f7e07f37ceb82fc3e56f3372c4c56bcee147f10
Machinec <1585645621@qq.com>
2020-04-16 23:35:36 +0800

    WebMvcConfiguration.java

--- src/main/java/com/a225/diseaseshow/contronller/DiseaseController.java
+++ src/main/java/com/a225/diseaseshow/contronller/DiseaseController.java
@@ -5,6 +5,7 @@ import com.a225.diseaseshow.bean.ResultRes;
 import com.a225.diseaseshow.service.DiseaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
